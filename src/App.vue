1528a2e6288f7f802d3feece786b23bb5823105f
Hazael Vite <14224891+HazaelVite@users.noreply.github.com>
2023-06-20 23:02:33 -0600

    Limpieza de proyecto y header

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,11 @@
 <script setup>
-
+import Header from './components/Header.vue';
 </script>
 
 <template>
+    <div class="container mx-auto mt-10 lg:mt-20">
+        <Header />
+    </div>
 
 </template>
 
---
+++ src/components/Header.vue
@@ -0,0 +1,6 @@
+<template>
+    <h1 class="text-5xl font-bold text-center">
+        Seguimiento Paciente
+        <span class="text-indigo-600">Veterinaria</span>
+    </h1>
+</template>
