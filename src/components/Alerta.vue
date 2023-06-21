4fb654aef9eed49dcd1664540cbd0a69214b9722
Hazael Vite <14224891+HazaelVite@users.noreply.github.com>
2023-06-21 06:53:42 -0600

    Dinamismo a la alerta

--- src/components/Alerta.vue
+++ src/components/Alerta.vue
@@ -1,17 +1,23 @@
 <script setup>
+import { computed } from "vue";
+
 const props = defineProps({
-    alerta: {
-        type: Object,
-        required: true
-    }
-})
+  alerta: {
+    type: Object,
+    required: true,
+  },
+});
+
+const isError = computed(() => {
+  return props.alerta.tipo === "error";
+});
 </script>
 
-
 <template>
-    <div 
-    class="text-white text-center uppercase font-bold bg-red-500 rounded-md p-3 mb-3"
-    >
-        {{ alerta.mensaje }}
-    </div>
+  <div
+    :class="[isError ? 'bg-red-500' : 'bg-green-500']"
+    class="text-white text-center uppercase font-bold rounded-md p-3 mb-3"
+  >
+    {{ alerta.mensaje }}
+  </div>
 </template>
